3b8b8463eb7c5d4466700abb5c19918af2b018de
Mallenahalli Shankara Lingappa Darshan <darshan.mallenahallishankaralingappa@aalto.fi>
2015-04-15 23:09:21 +0300

    Used nth_element

--- mf1/mf.cc
+++ mf1/mf.cc
@@ -1,17 +1,13 @@
 #include "mf.h"
 #include <iostream>     
 #include <algorithm>    
-#include <vector>  
+#include <set>
 using namespace std;
-
-int compare (const void * a, const void * b)
-{
-  return ( *(float*)a - *(float*)b );
-}
 
 void mf(int ny, int nx, int hy, int hx, const float* in, float* out) {
     int t,b,l,r,index =0;
-    float arr[(2*hx+1)*(2*hy+1)];
+    
+    float temp,arr[(2*hx+1)*(2*hy+1)];
     for (int y = 0; y < ny; ++y) 	
     	for (int x = 0; x < nx; ++x) 
    	{
@@ -19,23 +15,27 @@ void mf(int ny, int nx, int hy, int hx, const float* in, float* out) {
 		b = min(ny-1,y+hy);
  		l = max(0,x-hx);
 		r = min(nx-1,x+hx);
-		//n = (b-t+1)*(r-l+1);
-//printf("%d %d %d %d %d %d\n",x,y,t,b,l,r);
+
 		index=0;
  	        for(int j=t;j<=b;j++)
                    for(int i=l;i<=r;i++)
 			arr[index++] = in[i+j*nx];
-		 //std::nth_element(arr, arr + (index/2) ,arr+index);
-		//qsort (arr, index , sizeof(float), compare);
-		//out[x + nx*y] = arr[ index/2];
-		std::sort(arr,arr+index); 
+		std::nth_element(arr, arr + (index/2) ,arr+index);
+		temp = arr[index/2];
+//		printf("ind/2--%.1f ",temp);
 		if(index%2!=0)
-			out[x + nx*y] = arr[index/2];
+			out[x + nx*y] = temp;
 		else
-			out[x + nx*y] = (arr[index/2]+arr[(index/2)-1])/2;
+		{
+			std::nth_element(arr, arr + (index/2) - 1 ,arr+index);
+//			printf("ind/2-1--%.1f",arr[(index/2)-1]);
+			out[x + nx*y] = (temp + arr[(index/2)-1])/2.0;
+		}
     	}
 //	for (int y = 0; y < ny; ++y) 
-//   			printf("%.1f ",out[x + nx*y] );
+//	{
+//		for (int x = 0; x < nx; ++x) 
+//	 			printf("%.1f ",out[x + nx*y] );
 //		printf("\n");
 //	}
 //	printf("\n");
