8dd09a879d2c6b057e08de99e5265bbb3141d0a7
Mallenahalli Shankara Lingappa Darshan <darshan.mallenahallishankaralingappa@aalto.fi>
2015-04-22 00:25:47 +0300

    Initial Commit

--- cp1/cp.cc
+++ cp1/cp.cc
@@ -1,8 +1,44 @@
 #include "cp.h"
-
-void correlate(int ny, int nx, const float* data, float* result) {
-    // FIXME
-    for (int i = 0; i < ny * ny; ++i) {
-        result[i] = 0.0f;
-    }
+#include<math.h>
+void correlate(int ny, int nx, const float* data, float* result) 
+{
+	double* inter = new double[ny*nx];
+	for (int y = 0; y < ny; ++y) 
+	{
+		double mean = 0.0;
+		double sd = 0.0;
+		double temp;
+		//Finding the mean
+		for (int x = 0; x < nx; ++x) 
+		{
+			mean += data[x + y*nx];
+		}
+		mean = mean/nx;
+		//cout<<mean<<endl;
+		//Finding the Standard Deviation
+		for (int x = 0; x < nx; ++x) 
+		{
+			temp = data[x + y*nx] - mean;
+			inter[x + y*nx] = temp;
+			sd += (temp)*(temp);
+		}
+		sd= sqrt(sd);
+		//cout<<nx<<" "<<sd<<endl;
+		//Finding zero mean and unit variance
+		for (int x = 0; x < nx; ++x) 
+		{
+			inter[x + y*nx] = inter[x + y*nx] / sd;
+		}
+    	}
+	for (int i = 0; i < ny; i++) 
+	{
+        	for (int j = i ; j < ny; j++) 
+		{
+            		double sum = 0.0;
+	           	for (int k = 0; k < nx; k++)
+        		        sum = sum + inter[i * nx + k] * inter[j * nx + k];
+            		result[i * ny + j] = sum;
+        	}
+    	}
 }
+
